12475e2f1750b0459739273951ea99bccd32bbce
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2022-12-30 15:01:54 +0100

    added shot behaviour. Bullets generation to be fixed

--- src/views/MouseTracker.vue
+++ src/views/MouseTracker.vue
@@ -1,18 +1,26 @@
 <template>
-  <canvas ref="canvas" />
+  <div class="wrapper">
+    <canvas ref="canvas" @mousedown="repeat(shoot, 30)" @mouseup="onMouseUp" />
+    
+    <!-- Upper left info box -->
+    <div class="info">
+      <h3>Mouse tracker</h3>
+      <p>x: {{ mouse.x }} <br> y: {{ mouse.y }}</p>
+      <p>Fired: {{ bulletCounter }}</p>
+    </div>
 
-  <!-- Upper left info box -->
-  <div class="info">
-    <h3>Mouse tracker</h3>
-    <p>x: {{ mouse.x }} <br> y: {{ mouse.y }}</p>
-  </div>
-
-  <!-- Tank -->
-  <div class="abs-center tank"></div>
-
-  <!-- Bottom center tip -->
-  <div class="abs-center-bottom">
-    <h3>Press <span class="key">Esc</span> to come back to the menu</h3>
+    <!-- Top center tip -->
+    <div class="abs-center-top">
+      <h3>Click or hold-click to shoot</h3>
+    </div>
+    
+    <!-- Tank -->
+    <div class="abs-center tank"></div>
+    
+    <!-- Bottom center tip -->
+    <div class="abs-center-bottom">
+      <h3>Press <span class="key">Esc</span> to come back to the menu</h3>
+    </div>
   </div>
 </template>
 
@@ -29,6 +37,7 @@ import {
   getDirectionInRadians,
   getRandomColor,
   getXY,
+  getMagnitude,
 } from "../utils.js";
 
 // ==============================
@@ -37,6 +46,7 @@ import {
 const router = useRouter();
 const canvas = ref(undefined);
 const ctx = ref(undefined);
+const clearAllIntervals = ref( false );
 const offset = reactive({
   x: null,
   y: null,
@@ -47,63 +57,137 @@ const mouse = reactive({
   magnitude: null,
   direction: null
 })
-const line = reactive({
+const cannon = reactive({
   width: 100,
   height: 10,
-  color: '#fff'
+  color: '#fff',
+  interval: null,
 });
-let interval;
+const bullets = [];
+const bullet = {
+  id: null,
+  ref: null,
+  magnitude: 120,
+  speed: 5,
+  x: null,
+  y: null,
+  next: {
+    x: null,
+    y: null
+  },
+  interval: null,
+  color: null
+}
+
+let bulletCounter = 0;
+let bulletsInterval = null;
 
 // ==============================
 // Functions
 // ==============================
-function drawLine() {
+function shoot() {
+  // Update bullet counter
+  bulletCounter++;
+  
+  // Get current mouse direction
+  let actualDirection = mouse.direction;
+
+  // Create new bullet
+  bullets.push( {...bullet} );
+  bullets.at(-1).id = bulletCounter;
+  bullets.at(-1).color = getRandomColor();
+  
+  // Get ID of bullet (bullets's length may vary, an ID for every bullet is needed)
+  let id = bullets.map(b => b.id).indexOf( bulletCounter );
+
+  // Animate bullet with that ID
+  bullets.at( id ).interval = setInterval(() => {
+    updateBulletPosition( id, actualDirection );
+  }, 10);
+}
+
+function updateBulletPosition( id, direction ) {
+  // Calculate next bullet coordinates based on fixed direction
+  bullets.at(id).next = getXY({ mag: bullets.at(id).magnitude, dir: direction })
+  bullets.at(id).x = bullets.at(id).next.x + offset.x;
+  bullets.at(id).y = bullets.at(id).next.y + offset.y;
+  
+  // Create new one if doesn't exist
+  if ( !bullets.at(id).ref ) bullets.at(id).ref = document.createElement('div');
+  bullets.at(id).ref.style.cssText = `
+    width: 15px;
+    height: 3px;
+    background-color: ${ bullets.at(id).color };
+    position: absolute;
+    top: ${ bullets.at(id).y }px;
+    left: ${ bullets.at(id).x }px;
+    transform: translate(-50%, -50%) rotate(${ direction }rad);
+  `;
+  // Append to the DOM
+  document.querySelector('.wrapper').appendChild( bullets.at(id).ref );
+
+  // Update bullet magnitude
+  bullets.at(id).magnitude += bullets.at(id).speed;
+
+  if ( 
+    bullets.at(id).x > window.innerWidth || 
+    bullets.at(id).y > window.innerHeight ||
+    bullets.at(id).x < 0 || 
+    bullets.at(id).y < 0 ||
+    clearAllIntervals.value
+  ) {
+    clearInterval( bullets.at(id).interval );
+    
+    // Remove bullet from the DOM and from the bullets array
+    bullets.at(id).ref.remove();
+  }
+}
+
+function repeat( fun, int ){
+  bulletsInterval = setInterval(() => fun(), int )
+}
+
+function onMouseUp(){
+  clearInterval( bulletsInterval )
+}
+
+function drawCannon() {
   ctx.value.save();
 
   // Clear the canvas
   ctx.value.fillStyle = "#222";
   ctx.value.fillRect( -offset.x, -offset.y, canvas.value.width, canvas.value.height );
 
-  // Draw the line
+  // Draw the cannon
   ctx.value.beginPath();
   ctx.value.moveTo( 0, 0 );
- 
-  // Get current mouse direction
-  mouse.direction = getDirectionInRadians({ x: mouse.x, y: mouse.y });
   
   // Calculate new coordinates with fixed magnitude
-  let points = getXY({ mag: line.width, dir: mouse.direction });
+  let points = getXY({ mag: cannon.width, dir: mouse.direction });
 
-  // Draw the line (it ends to the new coordinates)
+  // Draw the cannon (it ends to the new coordinates)
   ctx.value.lineTo( points.x, points.y );
-  ctx.value.lineWidth = line.height;
-  ctx.value.strokeStyle = line.color;
+  ctx.value.lineWidth = cannon.height;
+  ctx.value.strokeStyle = cannon.color;
   ctx.value.lineCap = 'round';
   ctx.value.stroke();
-
-  // Draw bullets
-  // (?)
-  // 
-
   ctx.value.restore();
 }
 
-function drawPoint(x, y, radius = 5, color = "white") {
-  ctx.value.beginPath();
-  ctx.value.fillStyle = color;
-  ctx.value.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.value.fill();
-}
-
+// Clear the intervals, come back to the homepage
 function onkeyup(e) {
+  clearAllIntervals.value = true;
   if (e.key == "Escape") {
     router.push("/");
   }
 }
 
+// Update mouse x, y, magnitude, direction
 function onMouseMove(e) {
   mouse.x = e.clientX - offset.x;
   mouse.y = e.clientY - offset.y;
+  mouse.magnitude = getMagnitude({ x: mouse.x, y: mouse.y })
+  mouse.direction = getDirectionInRadians({ x: mouse.x, y: mouse.y });
 }
 
 // ==============================
@@ -123,13 +207,14 @@ onMounted(() => {
   ctx.value.translate(offset.x, offset.y);
   
   // Animate
-  interval = setInterval(() => drawLine(), 30);
+  cannon.interval = setInterval(() => drawCannon(), 30);
 });
 
 onUnmounted(() => {
   document.removeEventListener("keyup", onkeyup);
   document.removeEventListener("mousemove", onMouseMove);
-  clearInterval(interval);
+  clearInterval(cannon.interval);
+  clearInterval(bullet.interval);
 });
 </script>
 
