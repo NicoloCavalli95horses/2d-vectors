b6ed99c1b331861b5d8da1a8b1ab0da8efa7b3ec
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2024-03-24 12:38:00 +0100

    added multi-level

--- src/views/Shape.vue
+++ src/views/Shape.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="circle">
-    <div v-for="(p, i) in points" :key="i" class="dot" :style="{ 'left': `${p.x * 100}%`, 'top': `${p.y * 100}%` }" />
+    <template v-for="layer in points">
+      <div v-for="(p, i) in layer" :key="i" class="dot" :style="{ 'left': `${p.x * 100}%`, 'top': `${p.y * 100}%` }" />
+    </template>
   </div>
-  <div class="abs-center-bottom" @click="router.push('/')">
-    <h3>Back</h3>
-  </div>
+  <div class="abs-center-left pointer" @click="addLevel"><h3>Add level</h3></div>
+  <div class="abs-center-bottom pointer" @click="router.push('/')"><h3>Back</h3></div>
 </template>
 
 <script setup>
@@ -18,29 +19,21 @@ import GUI from "lil-gui";
 // ==============================
 // Consts
 // ==============================
-const router       = useRouter();
-const points       = ref([]);
-const gui          = new GUI();
-const radius       = 0.25;
-const step_degrees = 45;
-
-const guiObj = {
-  radius: radius,
-  step: step_degrees
-};
+const router      = useRouter();
+const points      = ref([]);
+const gui         = new GUI();
+const init_radius = ref(0.49);
+const init_step   = ref(25);
+const levels      = ref(0);
 
 const STEP_MIN   = 1;
 const STEP_MAX   = 180;
 const RADIUS_MIN = 0.100;
 const RADIUS_MAX = 0.49;
 
-gui.add(guiObj, "radius", RADIUS_MIN, RADIUS_MAX).onChange(val => calculatePointsOnCircle({ radius: val, step: step_degrees }));
-gui.add(guiObj, "step", STEP_MIN, STEP_MAX).onChange(val => calculatePointsOnCircle({ radius: radius, step: val }));
-
 // ==============================
-// Function
+// Functions
 // ==============================
-
 /**
  * 
  * @param {Object} 
@@ -48,7 +41,7 @@ gui.add(guiObj, "step", STEP_MIN, STEP_MAX).onChange(val => calculatePointsOnCir
  * @param {number} object.step   - define the step, in degrees, from 0 to 180°
  */
 function calculatePointsOnCircle({ radius, step }) {
-  points.value = [];
+  const points = [];
   const angleIncrement = step * Math.PI / 180; // from 0 to 2π
   const numPoints = Math.floor(360 / step);
   const c = { x: 0.5, y: 0.5 };
@@ -57,14 +50,41 @@ function calculatePointsOnCircle({ radius, step }) {
     let angle = angleIncrement * i;
     let x = radius * Math.cos(angle) + c.x;
     let y = radius * Math.sin(angle) + c.y;
-    points.value.push({ x, y });
+    points.push({ x, y });
   }
+  return points;
 }
+
+function onControlsChange({radius, step}, points) {
+  const _points = calculatePointsOnCircle({radius, step});
+  points.length = 0;
+  Array.prototype.push.apply(points, _points);
+}
+
+function addLevel() {
+  const newStep   = init_step.value; 
+  const newRadius = init_radius.value;
+  const newPoints = calculatePointsOnCircle({step: newStep, radius: newRadius});
+  const currentLevel = levels.value;
+  points.value.push(newPoints);
+  gui
+    .add({step: newStep, radius: newRadius}, "step", STEP_MIN, STEP_MAX)
+    .name(`Lv. ${levels.value} - step`)
+    .onChange(val => onControlsChange({ radius: newRadius, step: val }, points.value[currentLevel]));
+  gui
+    .add({step: newStep, radius: newRadius}, "radius", RADIUS_MIN, RADIUS_MAX)
+    .name(`Lv. ${levels.value} - radius`)
+    .onChange(val => onControlsChange({ radius: val, step: newStep }, points.value[currentLevel]));
+
+  levels.value++;
+  init_radius.value -= 0.05;
+}
+
 
 // ==============================
 // Life cycle
 // ==============================
-calculatePointsOnCircle({ radius: radius, step: step_degrees });
+addLevel();
 
 </script>
 
